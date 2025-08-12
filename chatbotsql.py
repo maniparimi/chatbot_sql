bc9e609d7ad923436f991b3b949dec8c6a0b9280
FirstName LastName <username@amazon.com>
2025-08-12 14:46:04 -0700

    fix import

--- chatbotsql.py
+++ chatbotsql.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from langchain.agents import create_sql_agent
 from langchain.sql_database import SQLDatabase
 from langchain.agents.agent_types import AgentType
-from langchain.callbacks import StdOutCallbackHandler
+from langchain.callbacks import StreamlitCallbackHandler
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from sqlalchemy import create_engine
 import sqlite3
