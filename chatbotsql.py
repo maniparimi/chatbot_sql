b0c5d101bf8c2ea2aaba3aad82b0662339970f30
FirstName LastName <username@amazon.com>
2025-08-12 13:51:26 -0700

    setup db connection in streamlit ui

--- chatbotsql.py
+++ chatbotsql.py
@@ -11,3 +11,51 @@ from sqlalchemy import create_engine
 import sqlite3
 from langchain_groq import ChatGroq
 
+# steamlit setup
+st.set_page_config(page_title="Langchain chat with sql db", page_icon="&")
+st.title("Langchain chat with sql db")
+
+
+LOCALDB = "USE_LOCALDB"
+MYSQL="USE_MYSQL"
+
+radio_opt = ["use SQL ite3 database - studebt db", "Connect to SQL database"]
+selected_option = st.sidebar.radio(label = "Choose the DB", options=radio_opt)
+
+if radio_opt.index(selected_option) == 1:
+    db_uri = MYSQL
+    mysql_host = st.sidebar.text_input("Provide Mysql host")
+    mysql_user = st.sidebar.text_input("Mysql user")
+    mysql_password = st.sidebar.text_input("Mysql pasword", type="password")
+    mysql_db = st.sidebar.text_input("Mysql database")
+else:
+    db_uri = LOCALDB
+
+api_key = st.sidebar.text_input(label="Groq api key", type="password")
+
+if not db_uri:
+    st.info("please enter the db information")
+
+# llm model
+llm = ChatGroq(groq_api_key = api_key, model_name = "llama3-8b-8192", streaming=True)
+
+#db confugure
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri, mysql_host=None, mysql_user=None, mysql_password = None, my_sqldb = None):
+    if db_uri == LOCALDB:
+        dbfilepath = (Path(__file__).parent/"student.db").absolute()
+        print(dbfilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode = ro", uri=True)
+        return SQLDatabase(create_engine("sqlite://", creator=creator))
+    elif db_uri == MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("please provide all mysql connection details")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))
+    
+if db_uri == MYSQL:
+    db = configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else:
+    db = configure_db(db_uri)
+
+#Tool kit
